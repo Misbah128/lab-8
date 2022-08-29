56e0c4fab54e7ac31064e1aa461ac5acf40fcfa8
Misbah128 <109318564+Misbah128@users.noreply.github.com>
2022-08-28 21:18:53 -0500

    Add files via upload

---
+++ Ch8Pb3.py
@@ -0,0 +1,7 @@
+# Syed Hussain
+# 08/28/2022
+# Problem 3 :- Writing a function that takes a list and prints if the value 5 is in that list.
+def check_five(lst):  # Define function
+  if 5 in lst: # Iterate through list
+    print('Number 5 is present in the list') # print to console
+check_five([2,6,8,4,5]) #Call function & it will print automatically to console
